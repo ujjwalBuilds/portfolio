2d1ef181c03763a5d9f893d3b0f467c4a255a3d9
Ujjwal Mishra <ujjwalmishra1609@gmail.com>
2025-07-20 12:35:15 +0530

    Initialized basic Navbar

--- portfolio/app/page.jsx
+++ portfolio/app/page.jsx
@@ -1,5 +1,8 @@
-
 export default function Home() {
-  return <main>Home Page</main>
-  
+  return (
+    <>
+      <div className="text-accent p-4 rounded">HomePage</div>
+      <div className="text-accent p-4 rounded"> Yo</div>
+    </>
+  );
 }
---
+++ portfolio/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//components
+import Nav from "./nav";
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Ujjwal <span className="text-accent">Mishra</span>
+          </h1>
+        </Link>
+        {/* desktop Nav & hire me button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button> Hire me</Button>
+          </Link>
+        </div>
+
+        {/* mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
